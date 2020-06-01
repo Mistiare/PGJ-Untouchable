1cce369595b564465b95c66e461c3a7bdf728744
DanielNeale <danielneale01@gmail.com>
2020-06-01 18:33:28 +0100

    player jumps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 moveVector;
+    [SerializeField]
+    private float speed = 0;
+    [SerializeField]
+    private float jumpHeight = 0;
+    private bool grounded;
+
+
+    void FixedUpdate()
     {
+        moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVector.x = 1;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveVector.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVector.z = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVector.z = 1;
+        }       
+
+        transform.GetComponent<Rigidbody>().AddForce(moveVector * speed, ForceMode.Force);
+
         
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            transform.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.transform.CompareTag("Floor"))
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("Floor"))
+        {
+            grounded = false;
+        }
     }
 }
