6bc517a43049320d7efab057dd7f85747ccfaab7
Callum Metcalfe <callummgamedev@hotmail.com>
2020-06-01 17:51:08 +0100

    Enemies will now go to waypoints

---
+++ Assets/Scripts/Enemies/EnemyPatrol.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField]
+    private Transform[] wayPoints;
+    private int destPoint;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(this.transform.position, transform.forward);
+    }
+
+    private void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        NextWayPoint();
+    }
+
+    private void Update()
+    {
+     if (!agent.pathPending && agent.remainingDistance <= 0.5f)
+        {
+            NextWayPoint();
+        }
+    }
+
+    private void NextWayPoint()
+    {
+        if (wayPoints.Length == 0)
+        {
+            return;
+        }
+        agent.destination = wayPoints[destPoint].position;
+
+        destPoint = (destPoint + 1) % wayPoints.Length;
+    }
+}
---
+++ Assets/Scripts/Enemies/WayPoints.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.1f, 1f)]
+    private float radius;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(this.transform.position, radius);
+    }
+}
+
