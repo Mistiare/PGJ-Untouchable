01755437da17d5169de90a5ac5cf5cc2281c39f7
DanielNeale <danielneale01@gmail.com>
2020-06-02 21:19:12 +0100

    Timer has been started

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    private float timer = 0;
+    private bool isTiming;
+
+
+    public void StartTimer()
+    {
+        isTiming = true;
+    }
+
+
+    void Update()
+    {
+        if (isTiming)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+
+    public void EndTimer()
+    {
+        isTiming = false;
+
+        string sceneName = SceneManager.GetActiveScene().name;
+        float currentScore = PlayerPrefs.GetFloat(sceneName);
+
+        if (currentScore == 0 || currentScore < timer)
+        {
+            PlayerPrefs.SetFloat(sceneName, timer);
+        }
+    }
+}
