5e05ca824ab317da04bfef151ba662368e5c13d2
Callum Metcalfe <callummgamedev@hotmail.com>
2020-06-02 14:41:40 +0100

    made it so stupid designer can see where the trigger area is :)

--- Assets/Scripts/TriggerArea.cs
+++ Assets/Scripts/TriggerArea.cs
@@ -6,6 +6,13 @@ public class TriggerArea : MonoBehaviour
 {
     [SerializeField]
     private int id;
+    private BoxCollider boxCollider;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(this.transform.position, this.GetComponent<BoxCollider>().size);
+    }
     private void OnTriggerEnter(Collider other)
     {
         GameEvents.current.EnemyTriggerEnter(id);
