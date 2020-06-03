24569e3b6f928681143c54ff6284cf3c8a9b7ebe
DanielNeale <danielneale01@gmail.com>
2020-06-03 15:16:36 +0100

    Camera now looks up and down

--- Assets/Scripts/NewCameraController.cs
+++ Assets/Scripts/NewCameraController.cs
@@ -8,6 +8,21 @@ public class NewCameraController : MonoBehaviour
     private Transform player = null;
     [SerializeField]
     private float rotateSpeed = 0;
+    [SerializeField]
+    private float raiseSpeed = 0;
+    [SerializeField]
+    private float maxHeight = 0;
+    [SerializeField]
+    private float minHeight = 0;
+    [SerializeField]
+    private float targetHeight = 0;
+    private Transform cam;
+
+
+    void Start()
+    {
+        cam = transform.GetChild(0);
+    }
 
 
     void Update()
@@ -16,5 +31,19 @@ public class NewCameraController : MonoBehaviour
 
         float newY = Input.GetAxis("Mouse X") * rotateSpeed;
         transform.Rotate(new Vector3(0, newY, 0));
+
+        float newX = Input.GetAxis("Mouse Y") * raiseSpeed;
+
+        if (cam.position.y < maxHeight && newX > 0)
+        {
+            cam.position += new Vector3(0, newX, 0);
+        }
+
+        if (cam.position.y > minHeight && newX < 0)
+        {
+            cam.position += new Vector3(0, newX, 0);
+        }
+
+        cam.LookAt(new Vector3(transform.position.x, targetHeight, transform.position.z));
     }
 }
