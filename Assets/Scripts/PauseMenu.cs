a945b6268aec02f1407cdbb80f1e67d06e58bf62
Callum Metcalfe <callummgamedev@hotmail.com>
2020-06-04 14:34:56 +0100

    Created basic pause menu includes -continue -menu -quit buttons

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pauseMenu = null;
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
+    }
+
+    public void Continue()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
