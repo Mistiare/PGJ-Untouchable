60237abf8ad35fcbed42ab67fc5ff5b66932f2c5
Elise Dennis <elisedennisdev@gmail.com>
2020-06-06 21:01:33 +0100

    -dont worry about this x2

---
+++ Assets/Scripts/seashanty.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seashanty : MonoBehaviour{
+
+    public int SeaShantyProb;
+    [SerializeField]
+    public AudioSource audioSource;
+    [SerializeField]
+    public AudioClip seaShanty;
+
+    public void Start()
+    {
+        RandomNumber();
+    }
+
+
+    public void RandomNumber()
+    {
+        SeaShantyProb = Random.Range(1, 2000);
+
+        if (SeaShantyProb == 13)
+        {
+            audioSource.PlayOneShot(seaShanty);
+        }
+    }
+
+
+
+}
