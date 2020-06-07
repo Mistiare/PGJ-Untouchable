e1c074027f22db5dcb1f5bfc1a3f94bc532a169e
DanielNeale <danielneale01@gmail.com>
2020-06-07 23:30:47 +0100

    Fuck particles - Dan edition

--- Assets/Scripts/PlayerDead.cs
+++ Assets/Scripts/PlayerDead.cs
@@ -7,30 +7,47 @@ public class PlayerDead : MonoBehaviour
     private GameObject gameOver = null;
     [SerializeField]
     private GameObject player = null;
+    [SerializeField]
+    private GameObject deathParticle = null;
+    private Vector3 position;
+    private Quaternion rotation;
+    private bool oneShot;
 
     private void Start()
     {
         gameOver.SetActive(false);
+        oneShot = true;
     }
 
     private void Update()
     {
         if (player == null)
         {
-            KillPlayer();
+            gameOver.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(0);
+            }
+
+            if (oneShot)
+            {
+                GameObject particle = Instantiate(deathParticle, position, rotation);
+                particle.transform.GetComponent<ParticleSystem>().Play();
+
+                oneShot = false;
+            }
         }
-    }
-    private void KillPlayer()
-    {
-        Destroy(player);
-        gameOver.SetActive(true);
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        else
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            SceneManager.LoadScene(0);
+            position = player.transform.position;
+            rotation = player.transform.rotation;
         }
     }
 }
